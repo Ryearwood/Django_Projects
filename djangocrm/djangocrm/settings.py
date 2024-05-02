c42f2e08f6e0be98c5ff18f59d7ddaf6affb1163
Russell Yearwood <yearwoodrussel@gmail.com>
2024-05-02 14:51:23 -0700

    configure DB settings

--- djangocrm/djangocrm/settings.py
+++ djangocrm/djangocrm/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "website",
 ]
 
 MIDDLEWARE = [
@@ -59,8 +60,12 @@ WSGI_APPLICATION = 'djangocrm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'crm_project',
+        'USER': 'root',
+        'PASSWORD': '****',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
