c1dc28adcc2f46e802c48ca25389de6c20979a63
Russell Yearwood <yearwoodrussel@gmail.com>
2024-05-02 17:03:36 -0700

    Add login and logout notifications + unverified authentication

--- djangocrm/website/views.py
+++ djangocrm/website/views.py
@@ -1,4 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
-    return render(request, 'home.html', {})
+    # CHeck to see if logging in
+    if request.method == "POST":
+        username = request.POST['user_name']
+        password = request.POST['password']
+        # Authenticate
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Login Successful!")
+            return redirect('home')
+        else:
+            messages.success(request, "Error Logging in, please try again...")
+            return redirect('home')
+    else:
+        return render(request, 'home.html', {})
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    pass
+
