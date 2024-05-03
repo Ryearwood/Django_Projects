351377bf1d411e403281fac866b191e14af6771d
Russell Yearwood <yearwoodrussel@gmail.com>
2024-05-03 15:00:47 -0700

    Add functionality to register new users for logging in and out of CRM

--- djangocrm/mydb.py
+++ djangocrm/mydb.py
@@ -1,14 +1,14 @@
 # Install Mysql on your computer
-# https://dev.mysql.com/downloads/installer/ or from CLI >> 
+# https://dev.mysql.com/downloads/installer/ or from CLI >>
 # 'brew install mysql'
 # 'brew services restart mysql'
-# 'mysql.server start' 
+# 'mysql.server start'
 # 'mysql_secure_installation'
 # If 'Access Denied' issue: Check here > https://stackoverflow.com/questions/41645309/mysql-error-access-denied-for-user-rootlocalhost
-# 'sudo mysql -u root -p" >> 
+# 'sudo mysql -u root -p" >>
 # Set password for root user
 # 'pip install mysql-connector-python'
-# 'pip install pymysql >> 
+# 'pip install pymysql >>
 # Add the following to the init.py file in root folder(same folder as settings.py):
 #   import pymysql
 #   pymysql.install_as_MySQLdb()
@@ -17,11 +17,7 @@
 
 import mysql.connector
 
-database = mysql.connector.connect(
-    host = '127.0.0.1',
-    user = 'root',
-    passwd = '****'
-)
+database = mysql.connector.connect(host="127.0.0.1", user="root", passwd="****")
 
 # Prepare a cursor object
 cursorObject = database.cursor()
--- djangocrm/website/views.py
+++ djangocrm/website/views.py
@@ -1,27 +1,48 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from website.forms import SignUpForm
+
 
 def home(request):
-    # CHeck to see if logging in
+    # Check to see if logging in
     if request.method == "POST":
-        username = request.POST['user_name']
-        password = request.POST['password']
+        username = request.POST["username"]
+        password = request.POST["password"]
         # Authenticate
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
             messages.success(request, "Login Successful!")
-            return redirect('home')
+            return redirect("home")
         else:
             messages.success(request, "Error Logging in, please try again...")
-            return redirect('home')
+            return redirect("home")
     else:
-        return render(request, 'home.html', {})
+        return render(request, "home.html", {})
 
 
 def logout_user(request):
     logout(request)
     messages.success(request, "Logout Successfully")
-    return redirect('home')
+    return redirect("home")
 
+
+def register_user(request):
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # Authenticate and Login
+            username = form.cleaned_data["username"]
+            password1 = form.cleaned_data["password1"]
+            user = authenticate(request, username=username, password=password1)
+            login(request, user)
+            messages.success(request, "Registration Successful")
+            return redirect("home")
+        else:
+            messages.success(request, "Somethings went wrong... Please Try Again")
+            return render(request, "register.html", {"form": form})
+    else:
+        form = SignUpForm()
+        return render(request, "register.html", {"form": form})
